ee5a4243cbb1d15ab99aa5a7a0d2804aa5f0ece4
zdz8313 <zhangdezheng8313@163.com>
2019-12-12 10:15:07 +0800

    spring cloud 提供者 没有加熔断器之前的版本1.1

--- src/main/java/com/oracle/hrb/firstekprovider/web/UserController.java
+++ src/main/java/com/oracle/hrb/firstekprovider/web/UserController.java
@@ -1,17 +1,16 @@
 package com.oracle.hrb.firstekprovider.web;
 
 import com.oracle.hrb.firstekprovider.bean.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @GetMapping
-    public  Object getUser(HttpServletRequest request) {
+    public  Object getUser(HttpServletRequest request) throws InterruptedException {
         User user = new User();
         user.setName("张三");
         user.setPassword("123456");
@@ -20,5 +19,10 @@ public class UserController {
         //url显示路径
         user.setMsg(request.getRequestURL().toString());
         return  user;
+    }
+    @PostMapping
+    public String addUser(@RequestBody User user){
+        System.out.println("添加了用户"+ user );
+        return "添加用户成功";
     }
 }
